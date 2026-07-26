2094f12698dcacc8e7c359e14fd491ef0e05e03b
Austin Hunt <austinsketches@gmail.com>
2026-07-26 01:31:23 -0400

    update kind words / testimonials

--- src/data/services.js
+++ src/data/services.js
@@ -29,6 +29,62 @@ export const services = [
 export const testimonials = [
   {
     quote:
+      'Austin Hunt has continued to improve observability for Puppet configuration in Splunk Cloud stacks; he implemented monitoring and alerts for Puppet failures across the fleet and broke them into cohorts (SOAR, ES, ITSI, Noah, classic, etc.). He implemented monitoring and alerting for Puppet CPU and memory usage on cloud stacks. He is collaborating with the CO2 team on monitoring/alerting for software convergence success and duration. Austin continually innovates, creating agent skills for things like on-call summary report creation and identifying the best alerting thresholds for metrics/monitors. He analyzes MR data to identify contribution trends and correlates those trends with other operational data, like incident frequency. Austin, thanks for proactively and creatively improving your team\'s operations!',
+    author: 'Aaron H',
+    context: 'Manager, ACM, Splunk',
+    date: 'June 2026',
+  },
+  {
+    quote:
+      'Austin has helped the Security Engineering team several times over the last few weeks. This week he helped us fix a problem with the NIDS services in the staging environment. A week or two ago he helped me to troubleshoot problems with universal forwarder configs, related to encrypted secrets. Austin is always happy to help, and quick to help find answers. Thanks Austin!',
+    author: 'Richard W',
+    context: 'Engineer, Security Engineering,Splunk',
+    date: 'February 2026',
+  },
+  {
+    quote:
+      'Austin helped a lot throughout the data.pp refactor project by contributing various refactor MRs. He also helped develop an AI skill to make these refactors easier and much faster. His skill to incorporate other skills to create Jiras, refactor the code, create MRs and finally add manual validation to the MR has been really useful for the team. Thanks for all your help and your Play to Win attitude which helped bring this project to completion.',
+    author: 'Ishan J',
+    context: 'Teammate, ACM, Splunk',
+    date: 'February 2026',
+  },
+  {
+    quote:
+      'Austin did a great job driving the contribution model Skynet dashboards for the control repo. These metrics clearly communicate the performnace and outcomes of the contribution model by tracking goals, SLAs, and the current state, and they are also being used by other teams to quickly adopt the model.',
+    author: 'Raman V',
+    context: 'Teammate, ACM, Splunk',
+    date: 'February 2026',
+  },
+  {
+    quote:
+      'ACM is refactoring control-repo so that important config modules are isolated from one another. This reduces blast radius of changes and allows ACM to grant fine-grained code ownership to contributors. Over the past few months, ACM has granted code ownership to five additional teams. Now, ACM has a dashboard to help us track improvements related to these changes. Since August 2025, we have seen a 20 percent increase in control-repo MRs merged without our involvement, which represents increased contributor independence and decreased time spent by ACM on MR reviews. And, our new incident trends dashboard shows no increase in related incidents. Thanks to Noah Woodin, Austin Hunt, and Raman Vasikarla for driving these code ownership and observability projects!',
+    author: 'Aaron H',
+    context: 'Manager, ACM, Splunk',
+    date: 'December 2025',
+  },
+  {
+    quote:
+      'ACM manages the rollout of Puppet configuration changes into all environments, including LVE, IL2, and IL5. While rollouts to the LVE fleet have long been done in phases, rollouts in IL2 and IL5 have gone to the entire fleet at once. This has allowed critical bugs to affect all customers in those environments. Austin Hunt has implemented phased rollouts for the IL2 fleet. Now, if a bug is introduced, it can be caught in the early cohorts, such as the non-customer canary stacks. Then, the rollout stops before it reaches customer stacks. This is a huge win for our IL2 customers! Thanks to Austin for driving this effort! We look forward to announcing the release of a phased rollout for the IL5 fleet soon.',
+    author: 'Aaron H',
+    context: 'Manager, ACM, Splunk',
+    date: 'November 2025',
+  },
+  {
+    quote:
+      'Thank you so much Austin for your perseverance and thoughtfulness in our recent issue. Thank you for connecting the dots and getting me information that I feel will be absolutely vital to my mission here at Cisco. Also, welcome aboard!',
+    author: 'Aaron T',
+    context: 'Engineer, Splunk',
+    date: 'October 2025',
+  },
+  {
+    quote:
+      'Control-repo is the home of the Puppet codebase, which manages the configuration of Splunk Enterprise Cloud (SEC) stacks. Setting up and running unit tests locally for Puppet development can be time-consuming and error-prone, especially when managing Ruby versions, dependencies, and system-specific setup. The ACM team has implemented a container-based approach for running RSpec locally, similar to how the control-repo CI pipeline executes unit tests. This approach helps new developers get started quickly and reduces friction in day-to-day testing. Thanks to Austin Hunt for driving the implementation. Thanks for Raman Vasikarla for design work, and thanks to Rick Rune for guidance!',
+    author: 'Aaron H',
+    context: 'Manager, ACM, Splunk',
+    date: 'August 2025',
+  },
+  {
+    quote:
       'Austin was incredibly helpful and patient when working with me. He guided me through any issues I had and was very reliable to work with.',
     author: 'Luke',
     context: 'Freelance client, Upwork',
